8d4e71c96a99b726d40aea1af3a22a854e3af14b
Beatsleigher <Beatsleigher@hotmail.com>
2014-03-29 02:29:08 +0100

    Added new screenshot, showing user listing capability.

---
+++ BukkitUI/run.bat
@@ -0,0 +1,11 @@
+@echo off 
+title BukkitUI by Beatsleigher
+color 0a
+goto :Start
+
+:Start
+echo BukkitUI is starting.
+echo If you find bugs or other errors, please report them on GitHub or the BukkitDev website.
+java -jar BukkitUI.jar
+echo BukkitUI has exited.
+pause
---
+++ BukkitUI/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Starting BukkitUI..."
+echo "Should any bugs or errors occur, please report them on GitHub or BukkitDev."
+java -jar BukkitUI.jar
+echo "BukkitUI has exited."
+echo "Hit ENTER to exit."
+read
